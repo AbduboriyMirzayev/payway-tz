ba469fcaec9d4f4de7b6c47b0b5e89fa3874b009
Abduboriy <abduboriy@Abduboriys-MacBook-Pro.local>
2024-05-27 21:44:35 +0500

    initialize ui components

---
+++ src/styles/index.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export default styled.main`
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: "Montserrat", sans-serif;
+  }
+`;
